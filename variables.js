aceff6a61b424dfb1738c807a38153cd6d7cf469
Yogesh S <yogesh87it@gmail.com>
2025-05-27 05:57:29 +0000

    variables basic

---
+++ variables.js
@@ -0,0 +1,13 @@
+const dateOfBirth = "19-10-1998";
+let country = "Albania";
+var city = "London";
+street = "24 Private";
+
+console.table([dateOfBirth,country,city,street]);
+
+dateOfBirth = "10-12-2003";//TypeError
+country = "Austria";
+city="New Yark";
+street = "charlie 56";
+
+console.table([dateOfBirth,country,city,street]);
