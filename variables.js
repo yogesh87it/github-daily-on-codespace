12c1bcb7210fcdfcdfaf896097cecb1dd5b9b639
Yogesh S <yogesh87it@gmail.com>
2025-05-27 06:12:51 +0000

    var behaviour in scope vs functions

--- variables.js
+++ variables.js
@@ -1,3 +1,4 @@
+// A Look Into Variables 
 const dateOfBirth = "19-10-1998";
 let country = "Albania";
 var city = "London";
@@ -5,7 +6,7 @@ street = "24 Private";
 
 console.table([dateOfBirth,country,city,street]);
 
-//dateOfBirth = "10-12-2003";//TypeError
+dateOfBirth = "10-12-2003";//TypeError
 country = "Austria";
 city="New Yark";
 street = "charlie 56";
@@ -24,4 +25,10 @@ if (myFriend==="Bob"){
     //fruit is printed as var does not respect the scope and available outside the scope
     //let and const respect the scope and will not be available outside it giving error;
 
-
+function checkFruit(){
+    var fruit = "orange";
+    let veg = "spinach";
+    console.log(fruit, veg);
+}
+    console.log(fruit); //Reference Error - fruit not defined
+//var respects function scope
